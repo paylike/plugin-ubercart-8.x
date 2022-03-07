7bd261d736e4bc03cf52e0442b2a9eabdf8c75a1
LucianDex <remindfwd@gmail.com>
2022-03-07 11:08:31 +0200

    Update quick_test.js

--- cypress/integration/quick_test.js
+++ cypress/integration/quick_test.js
@@ -35,32 +35,32 @@ describe('paylike plugin quick test', () => {
         TestMethods.changePaylikeCaptureMode(captureMode);
     });
 
-    // /**
-    //  * Change shop currency
-    //  */
-    // TestMethods.changeShopCurrencyFromAdmin(currency);
+    /**
+     * Change shop currency
+     */
+    TestMethods.changeShopCurrencyFromAdmin(currency);
 
-    // /** Pay and process order. */
-    // /** Capture */
-    // TestMethods.payWithSelectedCurrency(currency, 'capture');
+    /** Pay and process order. */
+    /** Capture */
+    TestMethods.payWithSelectedCurrency(currency, 'capture');
 
-    // /** Refund last created order (previously captured). */
-    // it('Process last order captured from admin panel to be refunded', () => {
-    //     TestMethods.processOrderFromAdmin('refund');
-    // });
+    /** Refund last created order (previously captured). */
+    it('Process last order captured from admin panel to be refunded', () => {
+        TestMethods.processOrderFromAdmin('refund');
+    });
 
-    // /** Partial Capture */
-    // TestMethods.payWithSelectedCurrency(currency, 'capture', /*partialAmount*/ true);
+    /** Partial Capture */
+    TestMethods.payWithSelectedCurrency(currency, 'capture', /*partialAmount*/ true);
 
-    // /** Refund last created order (previously captured). */
-    // it('Process last order captured from admin panel to be refunded', () => {
-    //     TestMethods.processOrderFromAdmin('refund', /*partialAmount*/ true);
-    // });
+    /** Refund last created order (previously captured). */
+    it('Process last order captured from admin panel to be refunded', () => {
+        TestMethods.processOrderFromAdmin('refund', /*partialAmount*/ true);
+    });
 
-    // /** Void */
-    // TestMethods.payWithSelectedCurrency(currency, 'void');
+    /** Void */
+    TestMethods.payWithSelectedCurrency(currency, 'void');
 
-    // /** Partial Void */
-    // TestMethods.payWithSelectedCurrency(currency, 'void', /*partialAmount*/ true);
+    /** Partial Void */
+    TestMethods.payWithSelectedCurrency(currency, 'void', /*partialAmount*/ true);
 
 }); // describe
